4a0c8e3d55e5c64441df1ece2b036cfd95596a76
Eric Groß <ericgross.eg@gmail.com>
2022-03-18 15:06:18 +0100

    Add lesson code

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,23 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Ninja App')
+          title: const Text('Ninja App'),
+          backgroundColor: Colors.red[600],
+        ),
+        body: const Center(
+          child: Text(
+            'Hello Ninjas',
+            style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2,
+                color: Colors.grey,
+                fontFamily: 'Roboto'),
           ),
-        body: Center(
-          child: Text('Hello Ninjas')
-          ),
-        floatingActionButton: FloatingActionButton( 
-          onPressed: () {
-        },
-          child: Text('click')
-          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.red[600],
+            onPressed: () {},
+            child: const Text('click')),
       ),
     ));
