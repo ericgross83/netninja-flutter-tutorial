5443f736561f81d5d39b0d3ea38a570e2dae3b8f
Eric Groß <ericgross.eg@gmail.com>
2022-03-18 15:25:11 +0100

    Add lesson code

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Ninja App'),
-          backgroundColor: Colors.red[600],
-        ),
-        body: const Center(
-          child: Text(
-            'Hello Ninjas',
-            style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2,
-                color: Colors.grey,
-                fontFamily: 'Roboto'),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-            backgroundColor: Colors.red[600],
-            onPressed: () {},
-            child: const Text('click')),
+void main() => runApp(const MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ninja App'),
+        backgroundColor: Colors.red[600],
       ),
-    ));
+      body: const Center(
+        child: Text(
+          'Hello people',
+          style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2,
+              color: Colors.grey,
+              fontFamily: 'Roboto'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.red[600],
+          onPressed: () {},
+          child: const Text('click')),
+    );
+  }
+}
